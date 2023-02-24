a3b5a6579ff451a982a6e2f3d1da3070db63aec4
Kardelen Canoğlu <kardelen.canoglu7@gmail.com>
2023-02-24 17:27:26 +0300

    v1 upload

---
+++ Assets/Scripts/UnitRTS.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitRTS : MonoBehaviour
+{
+
+    private GameObject selectedGameObject;
+    private IMovePosition movePosition;
+
+    private void Awake()
+    {
+        selectedGameObject = transform.Find("Selected").gameObject;
+        movePosition = GetComponent<IMovePosition>();
+        SetSelectedVisible(false);
+    }
+
+    public void SetSelectedVisible(bool visible)
+    {
+        selectedGameObject.SetActive(visible);
+    }
+
+    public void MoveTo(Vector3 targetPosition)
+    {
+        movePosition.SetMovePosition(targetPosition);
+    }
+
+}
